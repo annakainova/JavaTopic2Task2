3cb896056626e56873127745a37cd9b72b608851
Anna Kainova <annakainova88@gmail.com>
2023-02-07 12:04:31 +0100

    add output for bonus

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ public class Main {
 
         int balance = 100;
         int deposit = 1100;
+        int bonus = 0;
 
         if (deposit > 1000) {
-            balance += deposit + deposit / 100;
-        } else {
-            balance += deposit;
+            bonus = deposit / 100;
         }
 
+        balance += deposit + bonus;
         System.out.println("Balance: " + balance);
+        System.out.println("Bonus: " + bonus);
     }
 }
